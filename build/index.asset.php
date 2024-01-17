4f55ac3859800fad6f85f15842129a561fd8959c
thingalon <thingalon@users.noreply.github.com>
2024-01-17 09:20:38 +0000

    [Boost] Fix Modules e2e test (#35082)
    * Avoid re-fetching immediately after data is saved
    
    * changelog
    
    * Use a default staleTime of 1 second; it's enough to prevent unnecessary refetches
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7553399379

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react'), 'version' => 'd59377ebd1ae333453c6');
+<?php return array('dependencies' => array('react'), 'version' => '4b40235eb38ee9c3f160');
