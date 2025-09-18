e1ca2961859d494ba69ad63635c57d623fde5a0c
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-09-18 09:32:18 +0000

    Update dependency webpack to v5.101.3 (#45173)
    * Update dependency webpack to v5.101.3
    
    * Rename the file to fix the build error
    
    * Update test snapshots
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17824008701
    
    Upstream-Ref: Automattic/jetpack@18eb749eec2185dc106f74b36c163ef458dded0f

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-polyfill'), 'version' => 'af9a6b5962a0dd2f8da9');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-polyfill'), 'version' => '4934e5584a3a2fb2f0cc');
